ae293928fd32fdf607de9b416d48c109bfd858bf
Chandan Singh <74015019+chandan7953@users.noreply.github.com>
2023-08-12 03:36:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function decimalToBinary(num) {
   //Write you code here
+	let ans = [];
+      while(num != 0){
+        ans.push(num % 2);
+        num = Math.floor(num / 2);
+      }
+
+      let s = 0;
+      let e = ans.length - 1;
+      while(s < e){
+        [ans[s] , ans[e]] = [ans[e], ans[s]];
+        s++;
+        e--;
+      }
+      return ans.join("");
   
 }
 
